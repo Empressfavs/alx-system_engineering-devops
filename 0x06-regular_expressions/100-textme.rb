aa113b50c41f6f52170bcbe47f2c1632779f3c10
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-28 14:37:48 +0100

    a Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(//\[from:(?<sender>[\w+ ]+)\] \[to:(?<receiver>[\+\d]+)\] \[flags:(?<flags>[\d:-]+)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
